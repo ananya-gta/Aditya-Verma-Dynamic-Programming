76634d783056b4deaa1d362569c6c216563a3fd3
Ananya Gupta <anannya.gta@gmail.com>
2022-01-11 18:36:57 +0530

    Update Program.java

--- 0-1 Knapsack Problem/2. Equal Sum Partition/Program.java
+++ 0-1 Knapsack Problem/2. Equal Sum Partition/Program.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int num: nums) sum+= num;
+        if(sum%2!=0) return false;
+        else return subset_sum(nums, sum/2, nums.length);
+    }
+    public static boolean subset_sum(int nums[], int sum, int n)
+    {
+        boolean dp[][]= new boolean[n+1][sum+1];
+        for(int i=0; i<n+1; i++)
+            for(int j=0; j<sum+1; j++)
+            {
+               if(i==0) dp[i][j]=false;
+                else dp[i][j]= true;
+            }
+        for(int i=1; i<n+1; i++)
+            for(int j=1; j<sum+1; j++)
+            {
+                if(nums[i-1]<=j)
+                    dp[i][j]= dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        return dp[n][sum];
+    }
+}
