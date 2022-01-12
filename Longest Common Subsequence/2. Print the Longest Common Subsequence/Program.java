09ea24219feaa9af76c6e989a0aea76569266b01
Ananya Gupta <anannya.gta@gmail.com>
2022-01-12 21:17:11 +0530

    Update Program.java

--- Longest Common Subsequence/2. Print the Longest Common Subsequence/Program.java
+++ Longest Common Subsequence/2. Print the Longest Common Subsequence/Program.java
@@ -1,1 +1,45 @@
-
+// Dynamic Programming implementation of LCS problem in Java
+import java.io.*;
+ 
+class  LongestCommonSubsequence
+{
+    // Returns length of LCS for X[0..m-1], Y[0..n-1]
+    static void lcs(String X, String Y, int m, int n)
+    {
+        int[][] t = new int[m+1][n+1];
+  
+        // Following steps build L[m+1][n+1] in bottom up fashion. Note
+        // that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]
+        for (int i=0; i<=m; i++)
+        {
+            for (int j=0; j<=n; j++)
+            {
+                if (i == 0 || j == 0)
+                    t[i][j] = 0;
+                else if (X.charAt(i-1) == Y.charAt(j-1))
+                    t[i][j] = t[i-1][j-1] + 1;
+                else
+                    t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+            }
+        }
+      StringBuilder sb= new StringBuilder();
+      int i=m, j=n;
+      while(i>0 && j>0)
+      {
+        if(X.charAt(i-1) == Y.charAt(j-1))
+        {
+          sb.append(X.charAt(i-1));
+          i--;
+          j--;
+        }
+        else if(t[i][j-1] > t[i-1][j])
+          j--;
+        else
+          i--;  
+      }
+      sb.reverse();
+      return sb.toString();
+    }
+}
+        
+  
